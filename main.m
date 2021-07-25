365c2907d364b8be2b62dd67c5c548a488ab5498
unknown <arya.araban21@gmail.com>
2021-07-25 11:33:34 +0430

    made distance unit be CM and added dynamic frame detection

--- main.m
+++ main.m
@@ -1,15 +1,16 @@
 %clear;clc;
 v = VideoWriter('myFile.avi');
-rd1 = VideoReader('videos/hn1.avi');
-rd2 = VideoReader('videos/hn2.avi');
+rd1 = VideoReader('videos/hand_close1.avi');
+rd2 = VideoReader('videos/hand_close2.avi');
 v.VideoCompressionMethod
+numFrames = ceil(rd1.FrameRate*rd1.Duration)-5;
 open(v)
 %im = createOrangeMask(imread('C:\Users\Arya\Desktop\matlab-pp\pictures\games\1\100.png'))
 %imshow(single(im))
 worldPoints = [0 0 0];
-NUM_FRAMES = 50;
 x1=0;y1=0;
-for c = 1:NUM_FRAMES
+c = 1;
+while hasFrame(rd1)
     I1 = read(rd1,c);
     I2 = read(rd2,c);
     
@@ -23,12 +24,13 @@ for c = 1:NUM_FRAMES
         mp1 = [x1,y1];
         mp2 = [x2,y2];
         prevPoints = worldPoints; 
-        worldPoints = triangulate(mp1,mp2,stereoParams);
+        worldPoints = triangulate(mp1,mp2,stereoParams)/100;
         spd = norm(prevPoints-worldPoints);
         I1 = insertText(I1, [100 315 ], ['X: ' num2str(worldPoints(1)) ' Y: ' num2str(-worldPoints(2)) ' Z: ' num2str(worldPoints(3))]);
-        I1 = insertText(I1, [100 350 ], ['speed: ' num2str(spd) 'PPF']);
+        I1 = insertText(I1, [100 350 ], ['speed: ' num2str(spd) ' PPF']);
     end
     writeVideo(v,I1);
+    c = c + 1;
 end
 close(v)
-!ffmpeg -y -i videos/hn1.avi -i myFile.avi -filter_complex hstack -c:v ffv1 ./stiched.avi"
+!ffmpeg -y -i videos/hand_close1.avi -i myFile.avi -filter_complex hstack -c:v ffv1 ./stiched.avi"
