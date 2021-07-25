8c08eb5f35eef1fbf9458d1358d84de54fb693cf
unknown <arya.araban21@gmail.com>
2021-07-26 01:19:19 +0430

    optimized code to find centers

--- main.m
+++ main.m
@@ -1,10 +1,13 @@
 %clear;clc;
-v = VideoWriter('myFile.avi');
+v1 = VideoWriter('myFile.avi');
+%v2 = VideoWriter('myFile2.avi');
 rd1 = VideoReader('videos/hn1.avi');
 rd2 = VideoReader('videos/hn2.avi');
-v.VideoCompressionMethod
+v1.VideoCompressionMethod
+v2.VideoCompressionMethod
 numFrames = ceil(rd1.FrameRate*rd1.Duration)-5;
-open(v)
+open(v1)
+%open(v2)
 %im = createOrangeMask(imread('C:\Users\Arya\Desktop\matlab-pp\pictures\games\1\100.png'))
 %imshow(single(im))
 worldPoints = [0 0 0];
@@ -21,18 +24,22 @@ while hasFrame(rd1)
     [I1,x1,y1] = blob(I1);
     [I2,x2,y2] = blob(I2);
     %[x2,y2]
-    if x1 ~= 0
+    if ~isnan(x1)
         mp1 = [x1,y1];
         mp2 = [x2,y2];
         prevPoints = worldPoints; 
         worldPoints = triangulate(mp1,mp2,stereoParams)/10; % we divide by 10 to convert mm to cm 
-        spd = norm(prevPoints-worldPoints)* FPS/100; %divide by another 100 to find MPS else it'll be CMPS
+        spd = norm(prevPoints-worldPoints)* FPS/101; %divide by another 100 to find MPS else it'll be CMPS
         I1 = insertText(I1, [100 315 ], ['coords (cm): ' '[ X: ' num2str(worldPoints(1)) ' Y: ' num2str(-worldPoints(2)) ' Z: ' num2str(worldPoints(3)) ' ]']);
         I1 = insertText(I1, [100 350 ], ['speed: ' num2str(spd) ' Meters Per Sec']);
         
     end
-    writeVideo(v,I1);
+    writeVideo(v1,I1);
+   % writeVideo(v2,I2);
     c = c + 1;
 end
-close(v)
+close(v1)
+%close(v2)
+
 !ffmpeg -y -i videos/hn1.avi -i myFile.avi -filter_complex hstack -c:v ffv1 ./stiched.avi"
+%!ffmpeg -y -i videos/hn2.avi -i myFile.avi -filter_complex hstack -c:v ffv1 ./stiched2.avi"
