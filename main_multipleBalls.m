b773f39483c873c1e6cc912c971085169ffcfefa
unknown <arya.araban21@gmail.com>
2021-07-26 13:02:59 +0430

    able to specify number of balls(also find dist if two)

---
+++ main_multipleBalls.m
@@ -0,0 +1,53 @@
+%clear;clc;
+v = VideoWriter('myFile.avi');
+rd1 = VideoReader('videos/2b_1.avi');
+rd2 = VideoReader('videos/2b_2.avi');
+v.VideoCompressionMethod
+numFrames = ceil(rd1.FrameRate*rd1.Duration)-5;
+open(v)
+%im = createOrangeMask(imread('C:\Users\Arya\Desktop\matlab-pp\pictures\games\1\100.png'))
+%imshow(single(im))
+worldPoints = [0 0 0];
+FPS = 30;
+x1=0;y1=0;
+c = 1;
+
+a = [];
+text_y = 315;
+NUM_BLOBS = 2;
+
+while hasFrame(rd1)
+    I1 = read(rd1,c);
+    I2 = read(rd2,c);
+    
+    I1 =single(createBlueBallMask(I1));
+    I2 =single(createBlueBallMask(I2));
+    
+    [cnt_img1] = multipleblobs(I1,NUM_BLOBS);
+    [cnt_img2] = multipleblobs(I2,NUM_BLOBS);
+    
+    if isequal(size(cnt_img1),[NUM_BLOBS, 2]) && isequal(size(cnt_img2),[NUM_BLOBS, 2])
+        I1 = insertMarker(I1,cnt_img1,'x','color',{'green'},'size',20);
+        for i=1:NUM_BLOBS
+            mp1 = (cnt_img1(i,:));
+            mp2 = (cnt_img2(i,:));
+            worldPoints = triangulate(mp1,mp2,stereoParams)/10;
+            a = [a;worldPoints];
+            I1 = insertText(I1, [100 text_y], ['blob ' num2str(i, "%d") ' coords: ' '[ X: ' num2str(worldPoints(1)) ' Y: ' num2str(-worldPoints(2)) ' Z: ' num2str(worldPoints(3)) ' ]']);
+            text_y = text_y + 45;
+        end 
+        
+        if NUM_BLOBS == 2
+            dst = norm(a(1,:)-a(2,:));
+            I1 = insertText(I1, [100 100], ['distance: ' num2str(dst)]);
+            a = [];
+        end
+    %resetting values
+    text_y = 315;
+        
+    end
+    writeVideo(v,I1);
+    c = c + 1;
+end
+close(v)
+!ffmpeg -y -i videos/2b_1.avi -i myFile.avi -filter_complex hstack -c:v ffv1 ./stiched.avi"
--- multipleblobs.m
+++ multipleblobs.m
@@ -1,16 +1,10 @@
-function [out,centroids] = multipleblobs(bw)
-    out = bw;
-    X = []; Y = [];
+function [centroids] = multipleblobs(bw, num_blobs)
     labeledImage = logical(bw);
-    labeledImage = bwareafilt(labeledImage, 2, 'Largest');
+    labeledImage = bwareafilt(labeledImage, num_blobs, 'Largest');
     blobMeasurements = regionprops(labeledImage, 'Centroid');
     % We can get the centroids of ALL the blobs into 2 arrays,
     % one for the centroid x values and one for the centroid y values.
     centroids = cat(1,blobMeasurements.Centroid);
-    
-    if isequal(size(centroids),[2,2])
-    out = insertMarker(bw,centroids,'x','color',{'green'},'size',20);
-    end
 end 
 
 
