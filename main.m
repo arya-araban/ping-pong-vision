0209a8c76f1b540d4c6b9b80ac735435c4f4f66f
unknown <arya.araban21@gmail.com>
2021-08-22 13:58:51 +0430

    added the ability to see trajectory prediction for next frame we see

--- main.m
+++ main.m
@@ -1,8 +1,8 @@
 %clear;clc;
 v1 = VideoWriter('myFile.avi');
 %v2 = VideoWriter('myFile2.avi');
-rd1 = VideoReader('videos/7.1.avi');
-rd2 = VideoReader('videos/7.2.avi');
+rd1 = VideoReader('videos/5.1.avi');
+rd2 = VideoReader('videos/5.2.avi');
 v1.VideoCompressionMethod
 %v2.VideoCompressionMethod
 numFrames = ceil(rd1.FrameRate*rd1.Duration)-5;
@@ -14,18 +14,18 @@ prevPoints = [0 0 0];
 overall_worldpoints = zeros(numFrames+10,4); %stores balls location in frame seen: x,y,z, speed
 FPS = 30.0;
 
-FTC = FPS/1; %frames to consider per second -- to get every frame, put equal to FPS
+FTC = FPS/1; %frames to consider per second for speed -- to get every frame, put equal to FPS
 
 frames_speed = FPS/FTC;
 
-spd=0; missed_frames = 0;
+spd_total=0; missed_frames = 0;
 c = 1;
 while hasFrame(rd1)
     I1 = read(rd1,c);
     I2 = read(rd2,c);
     
-    I1 =single(createBlueBallMask(I1));
-    I2 =single(createBlueBallMask(I2));
+    I1 =single(createBlueBallMask22(I1));
+    I2 =single(createBlueBallMask22(I2));
     
     [cnt_img1] = blob(I1);
     [cnt_img2] = blob(I2);
@@ -36,18 +36,27 @@ while hasFrame(rd1)
         mp1 = (cnt_img1(1,:));
         mp2 = (cnt_img2(1,:));
        
-        overall_worldpoints(c,[1,2,3]) = worldPoints;
+        
         worldPoints = triangulate(mp1,mp2,stereoParams)/10; % we divide by 10 to convert mm to cm 
+        overall_worldpoints(c,[1,2,3]) = worldPoints;
         if rem(c,frames_speed) == 0
-            spd = norm(prevPoints-worldPoints)* (FTC/(missed_frames+1))/100; %divide by another 100 to find MPS else it'll be CMPS
+            %spd total is the speed considering all axis'. speed seperate
+            %has them seperately in each array 
+            
+            spd_total = norm(prevPoints-worldPoints)* (FTC/(missed_frames+1))/100; %divide by another 100 to find MPS else it'll be CMPS
+            
+            %now we look to predict trajectory by using trajectory formula x1=x0+(v*deltaT)
+            spd_seperate = abs(prevPoints-worldPoints)* (FTC/(missed_frames+1)); %keep this in CM since world points in CM
+            predicted_next_points = worldPoints + (spd_seperate * (1/FTC));
+            [num2str(c) '---current_world_points:' mat2str(worldPoints) 'predicted next points:' mat2str(predicted_next_points)]
             %reset the valus
             missed_frames =0; 
-            overall_worldpoints(c, (4)) = spd;
+            overall_worldpoints(c, (4)) = spd_total;
         %note that norm(prevPoints-worldPoints) unit is cmpf (cm per frame)
             prevPoints = worldPoints; 
         end  
-        I1 = insertText(I1, [100 315 ], ['coords (cm): ' '[ X: ' num2str(worldPoints(1)) ' Y: ' num2str(-worldPoints(2)) ' Z: ' num2str(worldPoints(3)) ' ]']);
-        I1 = insertText(I1, [100 350 ], ['speed: ' num2str(spd) ' Meters Per Sec']);
+        I1 = insertText(I1, [100 315 ], ['coords (cm): ' '[ X: ' num2str(worldPoints(1)) ' Y: ' num2str(worldPoints(2)) ' Z: ' num2str(worldPoints(3)) ' ]']);
+        I1 = insertText(I1, [100 350 ], ['speed: ' num2str(spd_total) ' Meters Per Sec']);
 
     else
         missed_frames = missed_frames + 1; 
@@ -58,8 +67,11 @@ end
 close(v1)
 
 %remove zero rows from WP's, and then first row(which is nonsensical speed)
-overall_worldpoints = overall_worldpoints(any(overall_worldpoints,2),:)
+overall_worldpoints = overall_worldpoints(any(overall_worldpoints,2),:);
 overall_worldpoints = overall_worldpoints((2:end),:);
+
+
+
 
 %plot 3d x y z's of the tracked ball
 plot3(overall_worldpoints(:,1),overall_worldpoints(:,2),overall_worldpoints(:,3))
@@ -71,5 +83,5 @@ overall_worldpoints = overall_worldpoints(logical(overall_worldpoints(:,4)),:);
 text(overall_worldpoints(:,1),overall_worldpoints(:,2),overall_worldpoints(:,3),num2cell(overall_worldpoints(:,4)))
 
 
-!ffmpeg -y -i videos/7.1.avi -i myFile.avi -filter_complex hstack -c:v ffv1 ./stiched.avi"
+!ffmpeg -y -i videos/5.1.avi -i myFile.avi -filter_complex hstack -c:v ffv1 ./stiched.avi"
 %!ffmpeg -y -i videos/hn2.avi -i myFile.avi -filter_complex hstack -c:v ffv1 ./stiched2.avi"
