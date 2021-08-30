0d4bdfa0d8dc1dc6c7f5a60cdaec4dacb787ed9e
unknown <arya.araban21@gmail.com>
2021-08-30 19:28:28 +0430

    added angle

--- blob.m
+++ blob.m
@@ -1,13 +1,11 @@
-function [centroids] = blob(bw)
-    out = bw;
-    x = nan; y = nan;
+function [centroids, angle] = blob(bw)
     labeledImage = logical(bw);
     labeledImage = bwareafilt(labeledImage, 1, 'Largest');
     blobMeasurements = regionprops(labeledImage, 'Centroid');
     % We can get the centroids of ALL the blobs into 2 arrays,
     % one for the centroid x values and one for the centroid y values.
     centroids = cat(1,blobMeasurements.Centroid);
- 
+    angle = regionprops(labeledImage, 'Orientation');
 end
 
 
